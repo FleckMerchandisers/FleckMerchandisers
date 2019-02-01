7cba80d6ed2864101a41b0a9f01ac3c8f2d88ef0
Marion Descamps <marion-zoe@hotmail.fr>
2019-02-01 18:45:27 +0100

    Backend : Login and Logout implemented

--- website/spareparts/views.py
+++ website/spareparts/views.py
@@ -8,6 +8,8 @@ from django.template import loader
 
 # Create your views here.
 
+from django.contrib.auth import logout
+
 from .models import Item
 
 from django.http import HttpResponse
@@ -17,11 +19,24 @@ from django.http import HttpResponse
 
 def index(request):
     latest_item_list = Item.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
+    template = loader.get_template('spareparts/index.html')
     context = {
         'latest_item_list': latest_item_list,
     }
     return HttpResponse(template.render(context, request))
 
 def detail(request, item_id):
-    return HttpResponse("You're looking at item %s." % item_id)
+    context = {
+        'item_id' : Item.objects.get(item_id)
+    }
+    template = loader.get_template('spareparts/detail.html')
+    return HttpResponse(template.render(context, request))
+
+def logout_view(request):
+    logout(request)
+    return index(request)
+
+def login_view(request):
+    context={}
+    template = loader.get_template('spareparts/login.html')
+    return HttpResponse(template.render(context, request))
