2ad4d3d7fc91cd71b05dee9e94f5ee032aef9063
Marion Descamps <marion-zoe@hotmail.fr>
2019-02-02 22:01:27 +0100

    Backend : adding image to item

--- website/PRI/settings.py
+++ website/PRI/settings.py
@@ -124,3 +124,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/spareparts/'
+
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
--- website/spareparts/models.py
+++ website/spareparts/models.py
@@ -14,6 +14,7 @@ class Item(models.Model):
   item_type = models.CharField(max_length=200)
   price = models.IntegerField(default=0)
   pub_date = models.DateTimeField(default=django.utils.timezone.now)
+  photo = models.ImageField(upload_to='images', default=None, null=True, blank=True)
 
   def __str__(self):
     return self.name
